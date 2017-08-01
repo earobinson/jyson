55c04b151b910f2c906a7f97d228586fd3a3e086
Julian Haldenby <j.haldenby@gmail.com>
2017-08-01 15:49:48 -0400

    Fixes for Issue #4 and Issue #5 (#7)
    * Setting up test case for resolving #5
    
    * Setting up test case for resolving #4
    
    * Fixing the Jyson library
    
    The library will now handle functions that return undefined.
    As well we will make sure that if a value returns false then
    we will not exit the loop.

---
+++ spec/lib/jyson/jyson.regression.spec.js
@@ -0,0 +1,43 @@
+const chai = require('chai');
+const expect = chai.expect;
+const jyson = require('./../../../lib/jyson');
+
+describe('jyson.regression.spec: avoiding regressions', () => {
+  beforeEach(() =>{
+    this.productTemplateFunction = jyson.buildTemplateFunction({
+      name: 'name',
+      countries: ['countries.$'],
+      biodegradeable: 'biodegradeable',
+      tags: ['tags.$']
+    });
+  });
+
+  it('must continue to output all properties after a property that evaluates false', () => {
+    const input = {
+      name: 'Bacon Peanut Butter “Ice Cream” for Dogs',
+      countries: ['Canada', 'USA'],
+      biodegradeable: false,
+      tags: [
+        'lactobacillus bulgaricus',
+        'enterocococcus thermophilus',
+        'lactobacillus acidophilus',
+        'bifidobacterium bifidum',
+        'lactobacillus casei'
+      ]
+    };
+
+    const json = this.productTemplateFunction(input);
+    expect(json).to.deep.equal({
+      name: 'Bacon Peanut Butter “Ice Cream” for Dogs',
+      countries: ['Canada', 'USA'],
+      biodegradeable: false,
+      tags: [
+        'lactobacillus bulgaricus',
+        'enterocococcus thermophilus',
+        'lactobacillus acidophilus',
+        'bifidobacterium bifidum',
+        'lactobacillus casei'
+      ]
+    });
+  });
+});
