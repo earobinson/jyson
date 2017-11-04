1c2e191909b425e473f0aa05fed9d60164e16b38
Edward Andrew Robinson <earobinson@gmail.com>
2017-11-04 18:03:48 -0400

    Added in a second test to get to 100% code coverage

--- spec/lib/jyson/jyson.error.spec.js
+++ spec/lib/jyson/jyson.error.spec.js
@@ -7,9 +7,28 @@ const expect = chai.expect;
 const jyson = require('./../../../lib/jyson');
 
 describe('jyson.error.spec: testing errors in jyson', () => {
-  it('must convert an object to "json"', () => {
+  it('must error if the template cant be parsed', () => {
     const templateFunction = jyson.buildTemplateFunction({
       key: undefined
+    });
+
+    return Q.when()
+    .then(() => {
+      return templateFunction({});
+    })
+    .then(() => {
+      return Q.reject('an error should have been thrown');
+    })
+    .catch(err => {
+      expect(err).to.equal('jyson encountered an unknown template value: undefined');
+    });
+  });
+
+  it('must error if the template cant be parsed thats in an array', () => {
+    const templateFunction = jyson.buildTemplateFunction({
+      key: [{
+        key: undefined
+      }]
     });
 
     return Q.when()
