477f76ddc6e5d82637ef35a063310f0f89906315
Edward Andrew Robinson <earobinson@gmail.com>
2017-11-20 20:11:47 -0500

    Jyson no longer relies on throws to detect the end of an array (#20)
    * Jyson no longer relies on throws to detect the end of an array
    
    - In the case that jyson can not find a $ your array will be empty
    - Jyson templates can now access two different arrays
    - Updated travis to cache node_modules and not send emails
    
    Resolves #15
    
    * Addressing comments:
    
    - https://github.com/hubba/jyson/pull/20#discussion_r152102841

--- changelog.md
+++ changelog.md
@@ -1,11 +1,16 @@
 # jyson changelog
 
+## v2.0.0
+- Jyson no longer throws an error to handle arrays it reads ahead to determine array length
+- A Jyson template can now access two different arrays
+- Resolves [Issue #15](https://github.com/hubba/jyson/issues/15)
+
 ## v1.3.0
 - Made jyson less dependent on lodash
-- Resolves [Issue #13](//hubba/jyson/issues/13)
+- Resolves [Issue #13](https://github.com/hubba/jyson/issues/13)
 
 ## v1.2.1
 - Fixed a bug when if an array was not provide when it was in the template jyson would crash
 
 ## v1.2.0
-- jyson now supports an array of objects Issue [Issue #9](//hubba/jyson/issues/9)
+- jyson now supports an array of objects Issue [Issue #9](https://github.com/hubba/jyson/issues/9)
--- spec/lib/jyson/jyson.error.spec.js
+++ spec/lib/jyson/jyson.error.spec.js
@@ -23,22 +23,85 @@ describe('jyson.error.spec: testing errors in jyson', () => {
     });
   });
 
-  it('must error if the template cant be parsed thats in an array', () => {
-    const templateFunction = jyson.buildTemplateFunction({
-      key: [{
-        key: undefined
-      }]
+  describe('arrays', () => {
+    it('json template arrays must be of length 1', () => {
+      const templateFunction = jyson.buildTemplateFunction({
+        key: ['a', 'b']
+      });
+
+      return Q.when()
+      .then(() => {
+        return templateFunction({});
+      })
+      .then(() => {
+        return Q.reject('an error should have been thrown');
+      })
+      .catch(err => {
+        expect(err.message).to.equal('jyson template arrays must be of length one at key: key');
+      });
     });
 
-    return Q.when()
-    .then(() => {
-      return templateFunction({});
-    })
-    .then(() => {
-      return Q.reject('an error should have been thrown');
-    })
-    .catch(err => {
-      expect(err).to.equal('jyson encountered an unknown template value: undefined');
+    describe('missing a $', () => {
+      it('must error in the simple case', () => {
+        const templateFunction = jyson.buildTemplateFunction({
+          key: [{
+            key2: undefined
+          }]
+        });
+
+        return Q.when()
+        .then(() => {
+          return templateFunction({});
+        })
+        .then(() => {
+          return Q.reject('an error should have been thrown');
+        })
+        .catch(err => {
+          expect(err).to.equal('jyson encountered an invalid array at: key');
+        });
+      });
+
+      it('must error in the simple case', () => {
+        const templateFunction = jyson.buildTemplateFunction({
+          fooKey: {
+            barKey: [{
+              key2: undefined
+            }]
+          }
+        });
+
+        return Q.when()
+        .then(() => {
+          return templateFunction({});
+        })
+        .then(() => {
+          return Q.reject('an error should have been thrown');
+        })
+        .catch(err => {
+          expect(err).to.equal('jyson encountered an invalid array at: barKey');
+        });
+      });
+
+      it('must error with nested keys', () => {
+        const templateFunction = jyson.buildTemplateFunction({
+          foo: [{
+            bar: {
+              key: undefined
+            }
+          }]
+        });
+
+        return Q.when()
+        .then(() => {
+          return templateFunction({});
+        })
+        .then(() => {
+          return Q.reject('an error should have been thrown');
+        })
+        .catch(err => {
+          expect(err).to.equal('jyson encountered an invalid array at: foo');
+        });
+      });
     });
   });
 });
